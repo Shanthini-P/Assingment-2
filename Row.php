1d281ab6642ab2009fbcd244fffbd480b93a6afe
Shanthini-P <saiparamasivam20@gmail.com>
2025-09-04 20:18:24 +0530

    Add files via upload

---
+++ Row.php
@@ -0,0 +1,11 @@
+<?php
+$matrix = [
+    [1, 2, 3],   // Row 1
+    [4, 5, 6],   // Row 2
+    [7, 8, 9]    // Row 3
+];
+
+ $value = $matrix[1][2];  
+
+echo "The value in the second row and third column is: " . $value;
+?>

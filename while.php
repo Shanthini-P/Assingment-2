239dc8c94142014da764ebc697dc4e514d940d3e
Shanthini-P <saiparamasivam20@gmail.com>
2025-09-04 20:47:20 +0530

    Add files via upload

---
+++ while.php
@@ -0,0 +1,15 @@
+<?php
+$fruits = ["Apple", "Banana", "Cherry", "Mango"];
+
+$i = 0;
+
+
+$length = count($fruits);
+
+
+while ($i < $length) {
+    echo "Element $i: " . $fruits[$i] . "<br>";
+    // Step 6: Increment counter
+    $i++;
+}
+?>
